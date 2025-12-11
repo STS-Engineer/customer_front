4fd3f6725c92db3f229fe73da022dc7c8c3e1f5b
STS-Engineer <administration.sts@avocarbon.com>
2025-12-11 11:47:15 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import CustomerManagement from './components/CustomerManagement';
 import Navbar from './components/Navbar.tsx';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
@@ -9,7 +10,18 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-
+     {/* ToastContainer with center position */}
+      <ToastContainer
+        position="top-center"  // Changed from "top-right" to "top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <main style={{ padding: '20px', marginTop: '60px' }}>
         <CustomerManagement />
       </main>
